caa8523b59bdaefa26d51cb7cbbd4f1c3cd4c490
noear <noear@live.cn>
2015-11-26 11:14:28 +0800

    添加命名接口的MD5实例

---
+++ ImageLoader.UWP/ImageLoader.UWP/Md5FileNameGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noear.UWP.Loader {
+    public class Md5FileNameGenerator : IFileNameGenerator {
+        public string Generate(string url) {
+            return Util.md5(url);
+        }
+    }
+}
