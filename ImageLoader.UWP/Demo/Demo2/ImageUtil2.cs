062e5d7cb40c8dc85105a45b5180a92ca7b7aa69
noear <noear@live.cn>
2015-11-26 12:26:27 +0800

    微调。

--- ImageLoader.UWP/Demo/Demo2/ImageUtil2.cs
+++ ImageLoader.UWP/Demo/Demo2/ImageUtil2.cs
@@ -11,41 +11,41 @@ using Windows.UI.Xaml.Media.Imaging;
 namespace Demo.Demo2 {
     public static class ImageUtil2 {
 
-public static ImageLoader Icon { get; private set; }
-public static ImageLoader Photo { get; private set; }
+        public static ImageLoader Icon { get; private set; }
+        public static ImageLoader Photo { get; private set; }
 
-//请在APP起动的时候调用
-public async static void TryInit() {
-    var options = new DisplayImageOptions.Builder()
-        .CacheInMemory(false)
-        .CacheOnDisk(true)
-        .Build();
+        //请在APP起动的时候调用
+        public async static void TryInit() {
+            var options = new DisplayImageOptions.Builder()
+                .CacheInMemory(false)
+                .CacheOnDisk(true)
+                .Build();
 
-    var root = ApplicationData.Current.LocalFolder;
-    //图标
-    var iconDir = await root.CreateFolderAsync("icon", CreationCollisionOption.OpenIfExists);
-    Icon = ImageLoader.Register("icon",new ImageLoaderConfiguration.Builder()
-                        .ThreadPoolSize(5)
-                        .DenyCacheImageMultipleSizesInMemory()
-                        .MemoryCache(new MemoryCache(10 * 1024 * 1024))
-                        .TasksProcessingOrder(QueueProcessingType.FIFO)
-                        .DiskCache(new DiskCache(iconDir, 100 * 1024 * 1024, new Md5FileNameGenerator()))
-                        .DefaultDisplayImageOptions(options)
-                        .ImageDownloader(new HttpClientImageDownloader())
-                        .Build());
+            var root = ApplicationData.Current.LocalFolder;
+            //图标
+            var iconDir = await root.CreateFolderAsync("icon", CreationCollisionOption.OpenIfExists);
+            Icon = ImageLoader.Register("icon", new ImageLoaderConfiguration.Builder()
+                                .ThreadPoolSize(5)
+                                .DenyCacheImageMultipleSizesInMemory()
+                                .MemoryCache(new MemoryCache(10 * 1024 * 1024))
+                                .TasksProcessingOrder(QueueProcessingType.FIFO)
+                                .DiskCache(new DiskCache(iconDir, 100 * 1024 * 1024, new Md5FileNameGenerator()))
+                                .DefaultDisplayImageOptions(options)
+                                .ImageDownloader(new HttpClientImageDownloader())
+                                .Build());
 
-    //像册
-    var photoDir = await root.CreateFolderAsync("photo", CreationCollisionOption.OpenIfExists);
-    Photo = ImageLoader.Register("photo", new ImageLoaderConfiguration.Builder()
-                        .ThreadPoolSize(5)
-                        .DenyCacheImageMultipleSizesInMemory()
-                        .MemoryCache(new MemoryCache(10 * 1024 * 1024))
-                        .TasksProcessingOrder(QueueProcessingType.FIFO)
-                        .DiskCache(new DiskCache(photoDir, new Md5FileNameGenerator()))
-                        .DefaultDisplayImageOptions(options)
-                        .ImageDownloader(new HttpClientImageDownloaderEx()) //使用自定义扩展支持高级用法
-                        .Build());
-}
+            //像册
+            var photoDir = await root.CreateFolderAsync("photo", CreationCollisionOption.OpenIfExists);
+            Photo = ImageLoader.Register("photo", new ImageLoaderConfiguration.Builder()
+                                .ThreadPoolSize(5)
+                                .DenyCacheImageMultipleSizesInMemory()
+                                .MemoryCache(new MemoryCache(10 * 1024 * 1024))
+                                .TasksProcessingOrder(QueueProcessingType.FIFO)
+                                .DiskCache(new DiskCache(photoDir, new Md5FileNameGenerator()))
+                                .DefaultDisplayImageOptions(options)
+                                .ImageDownloader(new HttpClientImageDownloaderEx()) //使用自定义扩展支持高级用法
+                                .Build());
+        }
 
         //===============
         //以下是高级用户
@@ -58,7 +58,8 @@ public async static void TryInit() {
                 .ExtraForDownloader(user)
                 .Build();
 
-            Photo.DownloadImage(url, options, (state, uri, view, img) => {
+            Photo.DownloadImage(url, options, (state, uri, view, img) =>
+            {
                 if (state == LoadingState.Completed) {
                     callback(img);
                 }
@@ -75,7 +76,7 @@ public async static void TryInit() {
                 if (extra != null) {
                     //把相关信息传过去作防盗验证
                     var user = extra as User;
-                    http.Header("UserID", user.UserID); 
+                    http.Header("UserID", user.UserID);
                     http.Header("Sex", user.Sex);
                     http.Header("APPID", "1111");
                     http.Header("APPMK", "XXXXXXXXXXXXXXXX");
