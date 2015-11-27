2b30833ec9dc97baa0c254be1941d3167e4a6318
noear <noear@live.cn>
2015-11-27 14:28:15 +0800

    取消对AsyncHttpClient.UWP的引用。进一步简化代码。 （有需要通过接口扩展）

--- ImageLoader.UWP/ImageLoader.UWP/HttpClientImageDownloader.cs
+++ ImageLoader.UWP/ImageLoader.UWP/HttpClientImageDownloader.cs
@@ -1,26 +1,12 @@
-﻿using Noear.UWP.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading.Tasks;
 using Windows.Storage.Streams;
+using Windows.Web.Http;
 
 namespace Noear.UWP.Loader {
     public class HttpClientImageDownloader : IImageDownloader {
-
-        protected virtual void build(AsyncHttpClient http, string url, object extra) {
-
-        }
-
-        public virtual async Task<IBuffer> download(string url, object extra) {
-            var httpClient = new AsyncHttpClient().Url(url);
-
-            build(httpClient, url, extra);
-
-            var rsp = await httpClient.Get();
-
-            return rsp.getBuffer();
+        public virtual Task<IBuffer> download(string url, object extra) {
+            return new HttpClient().GetBufferAsync(new Uri(url)).AsTask();
         }
     }
 }
