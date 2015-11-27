09aa0d57c49fd567a4ade1b945c9428cc67d1c9b
noear <noear@live.cn>
2015-11-27 15:07:22 +0800

    优化性能。

--- ImageLoader.UWP/ImageLoader.UWP/HttpClientImageDownloader.cs
+++ ImageLoader.UWP/ImageLoader.UWP/HttpClientImageDownloader.cs
@@ -5,8 +5,8 @@ using Windows.Web.Http;
 
 namespace Noear.UWP.Loader {
     public class HttpClientImageDownloader : IImageDownloader {
-        public virtual Task<IBuffer> download(string url, object extra) {
-            return new HttpClient().GetBufferAsync(new Uri(url)).AsTask();
+        public async Task<IBuffer> download(string url, object extra) {
+            return await new HttpClient().GetBufferAsync(new Uri(url));
         }
     }
 }
